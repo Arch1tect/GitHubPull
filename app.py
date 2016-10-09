b4a02c98b5e6ea160181ab4ec8e8f8d8dbb9843f
ArchITech <swtdavid@gmail.com>
2016-10-09 10:50:55 -0700

    created

---
+++ app.py
@@ -0,0 +1,27 @@
+from bottle import get, post, put, delete, route, request, run, template
+import json
+import git
+
+
+
+git_dir = "/Users/swotong/Project/Qtime"
+
+# dynamic routing
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+@post('/github')
+def githubUpdated():
+
+	g = git.cmd.Git(git_dir)
+	g.pull()
+
+
+	return "succeed!"
+
+
+
+
+
+run(host='0.0.0.0', port=8080)
